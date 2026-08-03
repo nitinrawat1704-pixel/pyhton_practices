00c204c4de6118a21bd1b9bc9b382dbe51fcfe41
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-03 12:25:19 +0530

    lists_practice.py

---
+++ lists.py
@@ -0,0 +1,13 @@
+# List of Hotels
+hotels = ["Taj", "Oberoi", "Radisson", "Hyatt", "Marriott"]
+# 1. Display the complete list of hotels.
+# 2. Add "ITC" at the end of the list.
+# 3. Insert "Le Meridien" at index 2.
+# 4. Remove "Hyatt" from the list.
+# 5. Remove the last hotel and display it.
+# 6. Find the index of "Radisson". 
+# 7. Add another "Taj" and count how many times it appears.
+# 8. Sort the hotels alphabetically.
+# 9. Reverse the hotel list.
+# 10. Copy the list into another variable and display both lists.
+
