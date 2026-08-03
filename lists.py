03b22229b7dee55cc9ebf855a1358b1aff5b5357
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-03 19:15:40 +0530

    Update lists.py

--- lists.py
+++ lists.py
@@ -37,3 +37,38 @@ hotels = ["Taj", "Oberoi", "Radisson", "Hyatt", "Marriott"]
 # 10. Copy the list into another variable and display both lists.
   old=hotels
   print(old)
+
+
+
+# Shopping Cart
+
+cart = ["Laptop", "Mouse", "Keyboard", "Monitor", "Mouse"]
+
+ -------------------------------------------------------
+
+# 1. Display all products.
+print(cart)
+# 2. Find the total number of products in the cart.
+print(len(cart))
+# 3. Check whether "Keyboard" exists in the cart.
+if "Keyboard" in cart:print("found")
+else:print("not found")
+# 4. Remove the first occurrence of "Mouse".
+cart.remove("Monitor")
+print(cart)
+# 5. Add "Headphones" and "Webcam".
+cart.extend(["Headphones","Webcam"])
+print(cart)
+# 6. Display the first and last products.
+print(cart[0],cart[-1])
+# 7. Count how many times "Mouse" appears.
+print(cart.count('Mouse'))
+# 8. Sort the products alphabetically.
+cart.sort()
+print(cart)
+# 9. Create a backup copy of the cart.
+back_up=cart
+print(back_up)
+# 10. Empty the cart.
+cart.clear()
+print(cart)
