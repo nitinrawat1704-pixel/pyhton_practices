1ced26b334558131bc958351d711fef61181f22e
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-11 21:25:39 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,25 @@
+1. Create a tuple containing 5 numbers and print it.
+2. Print the first element of a tuple.
+3. Print the last element of a tuple.
+4. Find the length of a tuple using len().
+5. Check whether a particular value exists in a tuple.
+6. Count how many times a value occurs using count().
+7. Find the index of a value using index().
+8. Find the maximum value using max().
+9. Find the minimum value using min().
+10. Find the total of tuple values using sum().
+
+
+
+1. marks = (20,45,68,85,59)
+   print(marks)
+2. print(marks[0])
+3. print(marks[-1])
+4. print(len(marks))
+5. for i in marks:
+     if i == 20: print("found")
+6. print(marks.count("h"))
+7. print(marks.index(85))
+8. max(marks)
+9. min(marks)
+10. sum(marks)
