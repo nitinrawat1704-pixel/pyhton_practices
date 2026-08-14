b018b18e3416227b1ae14cabee99888c60da4678
Nitin Rawat <nitinrawat1704@gmail.com>
2026-08-14 20:30:40 +0530

    Update practice_2.py

--- Dictionaries/practice_2.py
+++ Dictionaries/practice_2.py
@@ -6,6 +6,8 @@
 6. Remove the "age" key.
 7. Print all keys using keys().
 8. Print all values using values().
+9.Write a Python program to find and print all employees whose salary is greater than ₹85,000.
+
 
 1. print(student["name"])
 2. print(student["marks"])
@@ -19,3 +21,14 @@
   print(student) 
 7. for i in student.keys(): print(i)
 8. for i in student.values(): print(i)
+9.employees = {
+    101: {"name": "Nitin", "department": "IT", "salary": 90000},
+    102: {"name": "Ankit", "department": "Sales", "salary": 85000},
+    103: {"name": "Priya", "department": "Finance", "salary": 95000},
+    104: {"name": "Rahul", "department": "IT", "salary": 75000},
+    105: {"name": "Sneha", "department": "Finance", "salary": 88000}
+}
+
+for k,v in employees.items():
+    if v["salary"]>85000:print(v["name"],v["department"])
+    else:continue
